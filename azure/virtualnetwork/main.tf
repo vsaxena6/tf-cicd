4f735458a540a4453234c40ec21a1cd138577800
Vamsi <terraformingcloud@gmail.com>
2020-06-10 16:59:52 +0530

    Updated all

--- azure/virtualnetwork/main.tf
+++ azure/virtualnetwork/main.tf
@@ -3,20 +3,6 @@
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
 #
-# - Provider Block
-#
-
-provider "azurerm" {
-    version         =   ">=2.6"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
-    
-    features {}
-}
-
-#
 # - Create a Resource Group
 #
 
@@ -24,7 +10,7 @@ resource "azurerm_resource_group" "rg" {
     name        =   var.resource_group_name
     location    =   var.location
     tags        =   {
-        "project"       =   "Collabnix"
+        "author"        =   "Vamsi"
         "deployed_with" =   "Terraform"
     }
 }
@@ -39,7 +25,7 @@ resource "azurerm_virtual_network" "vnet" {
   location              =   azurerm_resource_group.rg.location
   address_space         =   [var.vnet_address_range]
    tags                 =   {
-        "project"       =   "Collabnix"
+        "author"        =   "Vamsi"
         "deployed_with" =   "Terraform"
     }
 }
--- variables.tf
+++ variables.tf
@@ -2,8 +2,24 @@
 #*                 Root Module                         *#
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
+# Service Principal Variables
 
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
+variable "client_id" {
+    description =   "Client ID (APP ID) of the application"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "Client Secret (Password) of the application"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "Subscription ID"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "Tenant ID"
+    type        =   string
+}
