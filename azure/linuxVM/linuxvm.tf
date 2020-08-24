08083d21b30963abfbe59e3893160c9635cb7a0d
Vamsi <terraformingcloud@gmail.com>
2020-08-24 13:57:58 +0530

    Added new modules

--- azure/linuxVM/linuxvm.tf
+++ azure/linuxVM/linuxvm.tf
@@ -80,7 +80,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                    =       "Tcp"
     source_port_range           =       "*"
     destination_port_range      =       22
-    source_address_prefix       =       "124.123.188.61" 
+    source_address_prefix       =       "*" 
     destination_address_prefix  =       "*"
     
     }
